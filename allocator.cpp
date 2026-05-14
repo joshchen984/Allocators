f0612f931c22b9660d2a10b8064541e3dc952c55
joshchen984 <joshchen984@gmail.com>
2026-05-14 12:53:18 -0400

    format files

--- allocator.cpp
+++ allocator.cpp
@@ -1,90 +1,96 @@
 #include "allocator.h"
-#include <sys/mman.h>
-#include <cstdint>
-#include <stdexcept>
 #include <cstdint>
 #include <new>
+#include <stdexcept>
+#include <sys/mman.h>
 
-Allocator::Allocator(std::uint32_t initial_capacity): heap{mmap(NULL, initial_capacity, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0)} {
-    if (heap == MAP_FAILED) {
-        throw std::runtime_error("mmap failed");
-    }
-    free_list = new (heap) ChunkInfo{nullptr, nullptr, initial_capacity - sizeof(ChunkInfo), false};
+Allocator::Allocator(std::uint32_t initial_capacity)
+    : heap{mmap(NULL, initial_capacity, PROT_READ | PROT_WRITE,
+                MAP_PRIVATE | MAP_ANONYMOUS, -1, 0)} {
+  if (heap == MAP_FAILED) {
+    throw std::runtime_error("mmap failed");
+  }
+  free_list = new (heap)
+      ChunkInfo{nullptr, nullptr, initial_capacity - sizeof(ChunkInfo), false};
 }
 
-void* Allocator::allocate(std::uint32_t size) {
-    ChunkInfo* cur = next_free_chunk(free_list, size);
-    if(cur == nullptr) {
-        throw std::bad_alloc();
-    }
-    cur->allocated = true;
-    if(should_split(cur->size, size)) {
-        splitChunk(cur, size);
-    }
-    if(free_list->allocated) {
-        free_list = next_free_chunk(cur->next);
-    }
-    return reinterpret_cast<char*>(cur) + sizeof(ChunkInfo);
-    
+void *Allocator::allocate(std::uint32_t size) {
+  ChunkInfo *cur = next_free_chunk(free_list, size);
+  if (cur == nullptr) {
+    throw std::bad_alloc();
+  }
+  cur->allocated = true;
+  if (should_split(cur->size, size)) {
+    splitChunk(cur, size);
+  }
+  if (free_list->allocated) {
+    free_list = next_free_chunk(cur->next);
+  }
+  return reinterpret_cast<char *>(cur) + sizeof(ChunkInfo);
 }
 
-void Allocator::free(void* ptr) {
-    if(ptr == nullptr) {
-        return;
+void Allocator::free(void *ptr) {
+  if (ptr == nullptr) {
+    return;
+  }
+  ChunkInfo *chunk = reinterpret_cast<ChunkInfo *>(static_cast<char *>(ptr) -
+                                                   sizeof(ChunkInfo));
+  chunk->allocated = false;
+  if (chunk->prev != nullptr && !chunk->prev->allocated) {
+    chunk->prev->size += chunk->size + sizeof(ChunkInfo);
+    chunk->prev->next = chunk->next;
+    if (chunk->next != nullptr) {
+      chunk->next->prev = chunk->prev;
     }
-    ChunkInfo* chunk = reinterpret_cast<ChunkInfo*>(static_cast<char*>(ptr) - sizeof(ChunkInfo));
-    chunk->allocated = false;
-    if(chunk->prev != nullptr && !chunk->prev->allocated) {
-        chunk->prev->size += chunk->size + sizeof(ChunkInfo);
-        chunk->prev->next = chunk->next;
-        if(chunk->next != nullptr) {
-            chunk->next->prev = chunk->prev;
-        }
-        chunk = chunk->prev;
+    chunk = chunk->prev;
+  }
+  if (chunk->next != nullptr && !chunk->next->allocated) {
+    chunk->size += chunk->next->size + sizeof(ChunkInfo);
+    if (chunk->next->next != nullptr) {
+      chunk->next->next->prev = chunk;
     }
-    if(chunk->next != nullptr && !chunk->next->allocated) {
-        chunk->size += chunk->next->size + sizeof(ChunkInfo);
-        if(chunk->next->next != nullptr) {
-            chunk->next->next->prev = chunk;
-        }
-        chunk->next = chunk->next->next;
-    }
-    if(free_list == nullptr || chunk < free_list) {
-        free_list = chunk;
-    }
+    chunk->next = chunk->next->next;
+  }
+  if (free_list == nullptr || chunk < free_list) {
+    free_list = chunk;
+  }
 }
 
-void Allocator::splitChunk(Allocator::ChunkInfo* chunk, std::uint32_t size) {
-    std::uint32_t remainingFreeSize = chunk->size - size - sizeof(ChunkInfo);
-    chunk->size = size;
-    ChunkInfo* remainingChunk = new (nextChunkLocation(chunk)) ChunkInfo{chunk, chunk->next, remainingFreeSize, false};
-    if(chunk->next) {
-        chunk->next->prev = remainingChunk;
-    }
-    chunk->next = remainingChunk;
+void Allocator::splitChunk(Allocator::ChunkInfo *chunk, std::uint32_t size) {
+  std::uint32_t remainingFreeSize = chunk->size - size - sizeof(ChunkInfo);
+  chunk->size = size;
+  ChunkInfo *remainingChunk = new (nextChunkLocation(chunk))
+      ChunkInfo{chunk, chunk->next, remainingFreeSize, false};
+  if (chunk->next) {
+    chunk->next->prev = remainingChunk;
+  }
+  chunk->next = remainingChunk;
 }
 
-bool Allocator::should_split(std::uint32_t chunkSize, std::uint32_t allocateSize) {
-    return chunkSize > allocateSize + sizeof(ChunkInfo);
+bool Allocator::should_split(std::uint32_t chunkSize,
+                             std::uint32_t allocateSize) {
+  return chunkSize > allocateSize + sizeof(ChunkInfo);
 }
 
-Allocator::ChunkInfo* Allocator::next_free_chunk(Allocator::ChunkInfo* startChunk) {
-    ChunkInfo* next_free = startChunk;
-    while(next_free != nullptr && next_free->allocated) {
-        next_free = next_free->next;
-    } 
-    return next_free;
+Allocator::ChunkInfo *
+Allocator::next_free_chunk(Allocator::ChunkInfo *startChunk) {
+  ChunkInfo *next_free = startChunk;
+  while (next_free != nullptr && next_free->allocated) {
+    next_free = next_free->next;
+  }
+  return next_free;
 }
 
-Allocator::ChunkInfo* Allocator::next_free_chunk(Allocator::ChunkInfo* startChunk, std::uint32_t size) {
-    ChunkInfo* cur = startChunk;
-    while(cur != nullptr &&
-            (cur->allocated || cur->size < size)) {
-       cur = cur->next; 
-    }
-    return cur;
+Allocator::ChunkInfo *
+Allocator::next_free_chunk(Allocator::ChunkInfo *startChunk,
+                           std::uint32_t size) {
+  ChunkInfo *cur = startChunk;
+  while (cur != nullptr && (cur->allocated || cur->size < size)) {
+    cur = cur->next;
+  }
+  return cur;
 }
 
-void* Allocator::nextChunkLocation(Allocator::ChunkInfo* chunk) {
-    return chunk + chunk->size + sizeof(ChunkInfo);
+void *Allocator::nextChunkLocation(Allocator::ChunkInfo *chunk) {
+  return chunk + chunk->size + sizeof(ChunkInfo);
 }
--- allocator.h
+++ allocator.h
@@ -3,22 +3,23 @@
 
 class Allocator {
 private:
-    struct ChunkInfo {
-        ChunkInfo* prev {};
-        ChunkInfo* next {};
-        std::uint32_t size {};
-        bool allocated {};
-    };
-    void* heap;
-    ChunkInfo* free_list;
-    
-    ChunkInfo* next_free_chunk(ChunkInfo* startChunk);
-    ChunkInfo* next_free_chunk(ChunkInfo* startChunk, std::uint32_t size);
-    bool should_split(std::uint32_t chunkSize, std::uint32_t allocateSize);
-    void* nextChunkLocation(ChunkInfo* chunk);
-    void splitChunk(ChunkInfo* chunk, std::uint32_t size);
+  struct ChunkInfo {
+    ChunkInfo *prev{};
+    ChunkInfo *next{};
+    std::uint32_t size{};
+    bool allocated{};
+  };
+  void *heap;
+  ChunkInfo *free_list;
+
+  ChunkInfo *next_free_chunk(ChunkInfo *startChunk);
+  ChunkInfo *next_free_chunk(ChunkInfo *startChunk, std::uint32_t size);
+  bool should_split(std::uint32_t chunkSize, std::uint32_t allocateSize);
+  void *nextChunkLocation(ChunkInfo *chunk);
+  void splitChunk(ChunkInfo *chunk, std::uint32_t size);
+
 public:
-    Allocator(std::uint32_t initial_capacity);
-    virtual void* allocate(std::uint32_t size);
-    virtual void free(void* ptr);
+  Allocator(std::uint32_t initial_capacity);
+  virtual void *allocate(std::uint32_t size);
+  virtual void free(void *ptr);
 };
